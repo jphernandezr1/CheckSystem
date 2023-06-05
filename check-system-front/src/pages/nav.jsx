d218c069ef15290c7b1be86df10fb49402ce5b36
jphernandezr1 <jp.hernandezr1@uniandes.edu.co>
2023-06-05 17:11:38 -0500

    V_final

--- check-system-front/src/pages/nav.jsx
+++ check-system-front/src/pages/nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
         <div className="flex items-center justify-between">
-            <a href="/" className="font-bold text-red-500 text-3xl">SplitMyBill</a>
+            <a href="/" className="font-bold text-blue-800 text-3xl">SplitMyBill</a>
             <a
                 href="/"
                 className="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm"
