b6f63b380d9f3f169c0436b61d137dad5b8d64fd
jphernandezr1 <jp.hernandezr1@uniandes.edu.co>
2023-06-05 20:39:00 -0500

    firebase

--- check-system-front/src/index.js
+++ check-system-front/src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />,
-);
+firebase.initializeApp({
+  apiKey: "AIzaSyBWmf3U_JX9VEuhlT4mcGc3I63Bi_b2Zzo",
+  authDomain: "split-my-bill-app.firebaseapp.com",
+  projectId: "split-my-bill-app",
+  storageBucket: "split-my-bill-app.appspot.com",
+  messagingSenderId: "225929682723",
+  appId: "1:225929682723:web:0733ba6561ca3b4aa83696",
+  measurementId: "G-NCZ6B2NNKZ",
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
